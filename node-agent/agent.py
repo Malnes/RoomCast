801e3179b36240d0e6a4ef712483894e77b5ad60
malnes <olamalnes@outlook.com>
2025-12-01 20:30:50 +0100

    Bump agent version to 0.3.3 in agent.py and main.py

--- node-agent/agent.py
+++ node-agent/agent.py
@@ -15,7 +15,7 @@ from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel, Field
 
 
-AGENT_VERSION = os.getenv("AGENT_VERSION", "0.3.2")
+AGENT_VERSION = os.getenv("AGENT_VERSION", "0.3.3")
 MIXER_CONTROL = os.getenv("MIXER_CONTROL", "Master")
 MIXER_FALLBACKS = [
     MIXER_CONTROL,
